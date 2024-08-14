12630c797d0eda5dffafb4827117282e372a1a72
Sargis681 <sargis.1998@mail.ru>
2024-08-14 19:27:08 +0400

    fixds

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 let numbers = [];
 
-for (let i = 0; i < 7; i++) {
+// Генерация 7 уникальных случайных чисел
+while (numbers.length < 7) {
   let randomNumber = Math.floor(Math.random() * 111) + 1; // Генерация числа от 1 до 111
-  numbers.push(randomNumber); // Добавление числа в массив
+  
+  // Проверка на уникальность
+  if (!numbers.includes(randomNumber)) {
+    numbers.push(randomNumber);
+  }
 }
 
 // Получаем элемент с id="numberList"
 const numberList = document.getElementById("numberList");
 
-// Создаем строку с элементами списка
+// Создаем строку с элементами
 let listHTML = '';
 
-numbers.forEach((number, i) => {
-    listHTML += `<div class="num"> Համար <li>${number}</li> 
-    <textarea id="w3review" name="w3review" rows="4" cols="50"></textarea>
-    </div>`; // Добавляем каждое число в строку HTML
-  });
+numbers.forEach((number) => {
+  listHTML += `<div class="num"> 
   
+    Համար:
+    <li>
+    ${number}
+    <li/>
+    <textarea name="description" rows="4" cols="50"></textarea>
+  </div>`;
+});
 
-  
-// Используем innerHTML для добавления всех <li> в <ul>
+// Используем innerHTML для добавления всех <div> в элемент с id="numberList"
 numberList.innerHTML = listHTML;
