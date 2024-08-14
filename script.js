709ce8b94b7a4397842e33b89ecac55fdb67314f
Sargis681 <sargis.1998@mail.ru>
2024-08-14 22:10:59 +0400

    fixds

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 let numbers = [];
+let numbersTwo = []
 
-// Генерация 7 уникальных случайных чисел
 while (numbers.length < 7) {
   let randomNumber = Math.floor(Math.random() * 111) + 1; // Генерация числа от 1 до 111
   
-  // Проверка на уникальность
   if (!numbers.includes(randomNumber)) {
     numbers.push(randomNumber);
   }
 }
+while (numbersTwo.length < 3) {
+  let randomNumberTwo = Math.floor(Math.random() * 19) + 1; // Генерация числа от 1 до 19
+  
+  if (!numbersTwo.includes(randomNumberTwo)) {
+    numbersTwo.push(randomNumberTwo);
+  }
+}
 
-// Получаем элемент с id="numberList"
 const numberList = document.getElementById("numberList");
+const numberListTwo = document.getElementById("numberListTwo");
 
-// Создаем строку с элементами
+
+
 let listHTML = '';
+let listHTMLTwo = '';
+
 
 numbers.forEach((number) => {
   listHTML += `<div class="num"> 
@@ -27,5 +36,15 @@ numbers.forEach((number) => {
   </div>`;
 });
 
-// Используем innerHTML для добавления всех <div> в элемент с id="numberList"
+numbersTwo.forEach((number) => {
+  listHTMLTwo += `<div class="num"> 
+  
+    Համար:
+    <li>
+    ${number}
+    <li/>
+    <textarea name="description" rows="4" cols="50"></textarea>
+  </div>`;
+});
+numberListTwo.innerHTML=listHTMLTwo
 numberList.innerHTML = listHTML;
